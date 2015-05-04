3ec53ff6ed19978ebcaed3ce4b3d59f2c3c7ace0
hargueta <hugoargueta94@gmail.com>
2015-05-04 16:55:14 -0700

    declared class

---
+++ Piece.h
@@ -0,0 +1,15 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+// ****************************
+// Class Name:
+//
+// Description:
+//
+// ****************************
+
+class Piece {
+
+};
+
+#endif // PIECE_H
