1d22f53922f49178f42abc4765f38503dfde3079
GabrielZapata <gabriel.pyramids@gmail.com>
2015-05-15 10:45:24 -0700

    added player to setup

--- Board.cpp
+++ Board.cpp
@@ -21,47 +21,56 @@ void Board::setup()
 			arra[6][iCol].setPieceIcon('p');
 			arra[6][iCol].setIsPiece(true);
 			arra[6][iCol].setIsPlayer1(true);
+			arra[6][iCol].setPlayer(1);
     }
 
     arra[7][0].setPiece(500);
     arra[7][0].setPieceIcon('r');
     arra[7][0].setIsPiece(true);
     arra[7][0].setIsPlayer1(true);
+    arra[7][0].setPlayer(1);
 
     arra[7][1].setPiece(300);
     arra[7][1].setPieceIcon('n');
     arra[7][1].setIsPiece(true);
     arra[7][1].setIsPlayer1(true);
+    arra[7][1].setPlayer(1);
 
     arra[7][2].setPiece(305);
     arra[7][2].setPieceIcon('b');
     arra[7][2].setIsPiece(true);
     arra[7][2].setIsPlayer1(true);
+    arra[7][2].setPlayer(1);
 
     arra[7][3].setPiece(200);
     arra[7][3].setPieceIcon('k');
     arra[7][3].setIsPiece(true);
     arra[7][3].setIsPlayer1(true);
+    arra[7][3].setPlayer(1);
 
     arra[7][4].setPiece(900);
     arra[7][4].setPieceIcon('q');
     arra[7][4].setIsPiece(true);
     arra[7][4].setIsPlayer1(true);
+    arra[7][4].setPlayer(1);
 
     arra[7][5].setPiece(305);
     arra[7][5].setPieceIcon('b');
     arra[7][5].setIsPiece(true);
     arra[7][5].setIsPlayer1(true);
+    arra[7][5].setPlayer(1);
 
     arra[7][6].setPiece(300);
     arra[7][6].setPieceIcon('n');
     arra[7][6].setIsPiece(true);
     arra[7][6].setIsPlayer1(true);
+    arra[7][6].setPlayer(1);
 
     arra[7][7].setPiece(500);
     arra[7][7].setPieceIcon('r');
     arra[7][7].setIsPiece(true);
     arra[7][7].setIsPlayer1(true);
+    arra[7][7].setPlayer(1);
 
 
     //player 2
@@ -71,47 +80,56 @@ void Board::setup()
         arra[1][iCol].setPieceIcon('P');
         arra[1][iCol].setIsPiece(true);
         arra[1][iCol].setIsPlayer2(true);
+        arra[1][iCol].setPlayer(2);
     }
 
     arra[0][0].setPiece(500);
     arra[0][0].setPieceIcon('R');
     arra[0][0].setIsPiece(true);
     arra[0][0].setIsPlayer2(true);
+    arra[0][0].setPlayer(2);
 
     arra[0][1].setPiece(300);
     arra[0][1].setPieceIcon('N');
     arra[0][1].setIsPiece(true);
     arra[0][1].setIsPlayer2(true);
+    arra[0][1].setPlayer(2);
 
     arra[0][2].setPiece(305);
     arra[0][2].setPieceIcon('B');
     arra[0][2].setIsPiece(true);
     arra[0][2].setIsPlayer2(true);
+    arra[0][1].setPlayer(2);
 
     arra[0][3].setPiece(200);
     arra[0][3].setPieceIcon('K');
     arra[0][3].setIsPiece(true);
     arra[0][3].setIsPlayer2(true);
+    arra[0][3].setPlayer(2);
 
     arra[0][4].setPiece(900);
     arra[0][4].setPieceIcon('Q');
     arra[0][4].setIsPiece(true);
     arra[0][4].setIsPlayer2(true);
+    arra[0][4].setPlayer(2);
 
     arra[0][5].setPiece(305);
     arra[0][5].setPieceIcon('B');
     arra[0][5].setIsPiece(true);
     arra[0][5].setIsPlayer2(true);
+    arra[0][5].setPlayer(2);
 
     arra[0][6].setPiece(300);
     arra[0][6].setPieceIcon('N');
     arra[0][6].setIsPiece(true);
     arra[0][6].setIsPlayer2(true);
+    arra[0][6].setPlayer(2);
 
     arra[0][7].setPiece(500);
     arra[0][7].setPieceIcon('R');
     arra[0][7].setIsPiece(true);
     arra[0][7].setIsPlayer2(true);
+    arra[0][7].setPlayer(2);
 }
 
 //the biggest function here.
